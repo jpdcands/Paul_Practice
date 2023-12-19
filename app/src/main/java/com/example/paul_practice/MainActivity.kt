dea239e8b535ed731f656a62ba7ee41a56e07319
Paul <jpdcands211@gmail.com>
2023-12-19 18:14:24 -0500

    Commit before adding Column

--- app/src/main/java/com/example/paul_practice/MainActivity.kt
+++ app/src/main/java/com/example/paul_practice/MainActivity.kt
@@ -1,56 +1,49 @@
 package com.example.paul_practice
 
 import android.os.Bundle
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface {
-                    MyScreen()
-                }
-            }
+            NumbersLazyColumn()
         }
     }
 }
 
-
 @Composable
-fun MyScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { /* TODO: Handle Pendulum Button Click */ }) {
-                Text("Pendulum")
-            }
-            Button(onClick = { /* TODO: Handle Posterior Stretching Button Click */ }) {
-                Text("Posterior Stretching")
-            }
+fun NumbersLazyColumn() {
+
+    val exerciseList = listOf("Pendulum", "Posterior Strething", "Up-the-back Stretch",
+        "Overhead Stretch", "Shoulder Flexor", "Shoulder Rotation", "Wall Climber-Side",
+        "Wall Climber-Front", "Shoulder Blade Squeeze", "Arm Reach- Front","Arm Reach-Side",
+        "Arm Raise-Side", "Shoulder Flexor and Extensor", "Wall Push-Up", "Scapular Retraction",
+        "Internal Rotator", "External Rotator")
+
+    LazyColumn {
+        items(items = exerciseList) { name ->
+            Text(
+                text = name,
+                modifier = Modifier.padding(26.dp)
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewUserCard() {
-    MyScreen()
+fun PreviewNumbersLazyColumn() {
+    NumbersLazyColumn()
 }
 
 
