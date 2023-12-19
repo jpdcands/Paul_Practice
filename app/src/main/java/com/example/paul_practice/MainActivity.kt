74a5479fff8edf014df2aa6ce6357395ab5c0e3b
Paul <jpdcands211@gmail.com>
2023-12-19 09:15:39 -0500

    3rd

--- app/src/main/java/com/example/paul_practice/MainActivity.kt
+++ app/src/main/java/com/example/paul_practice/MainActivity.kt
@@ -1,48 +1,58 @@
 package com.example.paul_practice
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.foundation.layout.Spacer
-
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.paul_practice.ui.theme.Paul_PracticeTheme
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(modifier = Modifier.fillMaxSize()) {
-                Column(verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "Hello")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = " Moosie2")
+            MaterialTheme {
+                Surface {
+                    MyScreen()
                 }
-
             }
         }
     }
 }
 
-/*@Preview(showBackground = true)
+
 @Composable
-fun MainActivityPreview() {
-    Column(
-        Text("Hello Moosie"),
-                Text ("You Rock")
-    )
-}*/
+fun MyScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { /* TODO: Handle Pendulum Button Click */ }) {
+                Text("Pendulum")
+            }
+            Button(onClick = { /* TODO: Handle Posterior Stretching Button Click */ }) {
+                Text("Posterior Stretching")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewUserCard() {
+    MyScreen()
+}
+
+
+
 
